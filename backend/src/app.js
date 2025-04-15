d7fa562406315c64aea18c694a0f3ed8d53d2278
JayaantKumar <112800009+JayaantKumar@users.noreply.github.com>
2025-04-15 23:44:43 +0530

    completed project

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const aiRoutes = require('./routes/ai.router')
+const cors = require('cors')
 
 const express = require('express');
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) =>{
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import "prismjs/themes/prism-tomorrow.css"
+import prism from "prismjs"
+import Editor from "react-simple-code-editor"
+import axios from 'axios'
+import Markdown from "react-markdown"
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [code, setcode] = useState("")
+
+  const [review, setReview] = useState(``)
+
+  useEffect(()=>{
+    prism.highlightAll()
+  }, [])
+
+  async function reviewCode() {
+    const response = await axios.post('http://localhost:3000/ai/get-review', {code})
+
+    setReview(response.data)
+  }
 
   return (
     <>
-      
+    <main>
+      <div className='left' >
+          <div className='code' >
+            <Editor
+              value={code}
+              onValueChange={code => setcode(code)}
+              highlight={code => prism.highlight(code, prism.languages.javascript, "javascript")}
+              padding={10}
+              style={{
+                fontFamily: '"Fira Code", "Fira Mono", monospace',
+                fontSize: 16,
+                border: "1px solid #ddd",
+                borderRadius:"5px",
+                height:"100%",
+                width:"100%"
+              }}
+
+              />
+            
+            
+          </div>
+          <div className='review'onClick={reviewCode}>Review</div>
+      </div>
+      <div className='right'>
+          <Markdown
+          rehypePlugins={[rehypeHighlight]}
+          >{review}</Markdown>
+      </div>
+    </main>
     </>
   )
 }
