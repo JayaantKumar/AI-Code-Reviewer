407212df3f938ff02180c06c905f0386b3c1cd2f
JayaantKumar <112800009+JayaantKumar@users.noreply.github.com>
2025-04-16 17:28:18 +0530

    port 5173

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const app = require('./src/app');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5173
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
