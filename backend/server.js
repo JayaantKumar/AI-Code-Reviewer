402e1b0b0f8fc0c00e5ff966a0e92069034b7455
JayaantKumar <112800009+JayaantKumar@users.noreply.github.com>
2025-04-16 19:22:23 +0530

    changes in package.json

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
-const port = process.env.PORT || 5173
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-  })
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Listening on port ${PORT}`);
+});
