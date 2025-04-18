97d114918b9788d2b9199d3edcb36dd39a7d52fd
JayaantKumar <112800009+JayaantKumar@users.noreply.github.com>
2025-04-18 11:52:49 +0530

    configured helmet property

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,26 @@
 require('dotenv').config()
 const aiRoutes = require('./routes/ai.router')
 const cors = require('cors')
+const helmet = require('helmet');
 
 const express = require('express');
 
 const app = express()
+
+
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      fontSrc: ["'self'", 'https://ai-code-reviewer-5jcc.onrender.com'],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      connectSrc: ["'self'"],
+      imgSrc: ["'self'"],
+    },
+  },
+}));
+
 
 app.use(cors())
 app.use(express.json())
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,65 +1,64 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import "prismjs/themes/prism-tomorrow.css"
-import prism from "prismjs"
-import Editor from "react-simple-code-editor"
-import axios from 'axios'
-import Markdown from "react-markdown"
+import { useEffect, useState } from "react";
+import "./App.css";
+import "prismjs/themes/prism-tomorrow.css";
+import prism from "prismjs";
+import Editor from "react-simple-code-editor";
+import axios from "axios";
+import Markdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
 import "highlight.js/styles/github-dark.css";
 
-
 function App() {
+  const [code, setcode] = useState("");
 
-  const [code, setcode] = useState("")
+  const [review, setReview] = useState(``);
 
-  const [review, setReview] = useState(``)
-
-  useEffect(()=>{
-    prism.highlightAll()
-  }, [])
+  useEffect(() => {
+    prism.highlightAll();
+  }, []);
 
   async function reviewCode() {
-    const response = await axios.post('https://ai-code-reviewer-5jcc.onrender.com/ai/get-review', {code})
+    const response = await axios.post(
+      "https://ai-code-reviewer-5jcc.onrender.com/ai/get-review",
+      { code }
+    );
 
-    setReview(response.data)
+    setReview(response.data);
     console.log(response.data);
-    
   }
 
   return (
     <>
-    <main>
-      <div className='left' >
-          <div className='code' >
+      <main>
+        <div className="left">
+          <div className="code">
             <Editor
               value={code}
-              onValueChange={code => setcode(code)}
-              highlight={code => prism.highlight(code, prism.languages.javascript, "javascript")}
+              onValueChange={(code) => setcode(code)}
+              highlight={(code) =>
+                prism.highlight(code, prism.languages.javascript, "javascript")
+              }
               padding={10}
               style={{
                 fontFamily: '"Fira Code", "Fira Mono", monospace',
                 fontSize: 16,
                 border: "1px solid #ddd",
-                borderRadius:"5px",
-                height:"100%",
-                width:"100%"
+                borderRadius: "5px",
+                height: "100%",
+                width: "100%",
               }}
-
-              />
-            
-            
+            />
           </div>
-          <div className='review'onClick={reviewCode}>Review</div>
-      </div>
-      <div className='right'>
-          <Markdown
-          rehypePlugins={[rehypeHighlight]}
-          >{review}</Markdown>
-      </div>
-    </main>
+          <div className="review" onClick={reviewCode}>
+            Review
+          </div>
+        </div>
+        <div className="right">
+          <Markdown rehypePlugins={[rehypeHighlight]}>{review}</Markdown>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
