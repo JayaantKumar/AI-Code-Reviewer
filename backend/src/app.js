b6052250ed74d3b48ba7eda33194294e03262ba9
JayaantKumar <112800009+JayaantKumar@users.noreply.github.com>
2025-04-25 10:43:21 +0530

    build .dist folder

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,8 @@ const express = require('express');
 
 const app = express()
 
+const path = require('path')
+
 
 app.use(helmet({
   contentSecurityPolicy: {
@@ -25,9 +27,13 @@ app.use(helmet({
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) =>{
-    res.send('Hello world')
-})
+// Serve static frontend files
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Catch-all for frontend routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use('/ai',aiRoutes)
 
